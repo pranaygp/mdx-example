0ca924aa6ca4e235ed20b3dbac2a91541fa3d155
timothyis <hello@timothy.is>
2018-06-05 23:38:04 +0100

    Add remark-capitalize to make all heading title-case

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const remarkCapitalize = require('remark-capitalize')
 const withMDX = require('@zeit/next-mdx')({
-  extension: /\.(md|mdx)?$/
+  extension: /\.(md|mdx)?$/,
+  options: {
+    mdPlugins: [remarkCapitalize],
+    hastPlugins: []
+  }
 })
 
 module.exports = withMDX()
